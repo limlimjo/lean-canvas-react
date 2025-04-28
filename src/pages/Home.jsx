f2edf192f2a52969177f9a0f948e3d6e8ef98ca2
limlimjo <skyblue.777@daum.net>
2025-04-28 21:22:30 +0900

    [add] 린 캔버스 검색 기능 구현

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,37 +1,27 @@
 import { useState, useEffect } from 'react';
-import axios from 'axios';
 import CanvasList from '../components/CanvasList';
 import SearchBar from '../components/SearchBar';
 import ViewToggle from '../components/ViewToggle';
 import { getCanvases } from '../api/canvas';
 
 function Home() {
-  const [searchText, setSearchText] = useState('');
+  const [searchText, setSearchText] = useState();
   const [isGridView, setIsGridView] = useState(true);
 
   const [data, setData] = useState([]);
 
-  async function fetchData() {
-    // const data = await fetch('http://localhost:8000/canvases')
-    //   .then(res => res.json())
-    //   .catch(console.error);
-    // const response = await axios.get('http://localhost:8000/canvases');
-    // console.log(response);
-    const response = await getCanvases();
+  async function fetchData(params) {
+    const response = await getCanvases(params);
     setData(response.data);
   }
 
   useEffect(() => {
-    fetchData();
-  }, []);
+    fetchData({ title_like: searchText });
+  }, [searchText]);
 
   const handleDeleteItem = id => {
     setData(data.filter(item => item.id !== id));
   };
-
-  const filteredItems = data.filter(item =>
-    item.title.toLowerCase().includes(searchText.toLowerCase()),
-  );
 
   return (
     <div className="container mx-auto px-4 py-16">
@@ -40,7 +30,7 @@ function Home() {
         <ViewToggle isGridView={isGridView} setIsGridView={setIsGridView} />
       </div>
       <CanvasList
-        filteredItems={filteredItems}
+        filteredItems={data}
         isGridView={isGridView}
         searchText={searchText}
         onDeleteItem={handleDeleteItem}
