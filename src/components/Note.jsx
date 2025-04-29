2d1d6df05070f51381d20c75ef87692fcb631813
limlimjo <skyblue.777@daum.net>
2025-04-29 20:56:08 +0900

    [add] 린 캔버스 상세 조회기능 구현

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,7 +1,7 @@
 import { useState, useRef, useEffect } from 'react';
 import { AiOutlineClose, AiOutlineCheck } from 'react-icons/ai';
 
-const Note = ({ id, onRemoveNote }) => {
+const Note = ({ id, content, color: initialColor, onRemoveNote }) => {
   const colorOptions = [
     'bg-yellow-300',
     'bg-pink-300',
@@ -9,13 +9,15 @@ const Note = ({ id, onRemoveNote }) => {
     'bg-green-300',
   ];
 
-  // 0, 1, 2, 3
-  const randomIndex = Math.floor(Math.random() * colorOptions.length);
-  const [color, setColor] = useState(colorOptions[randomIndex]);
+  const [color, setColor] = useState(() => {
+    if (initialColor) return initialColor;
+    // 0, 1, 2, 3
+    const randomIndex = Math.floor(Math.random() * colorOptions.length);
+    return colorOptions[randomIndex];
+  });
 
   const [isEditing, setIsEditing] = useState(false);
   const textareaRef = useRef(null);
-  const [content, setContent] = useState('');
 
   useEffect(() => {
     if (textareaRef.current) {
@@ -54,7 +56,6 @@ const Note = ({ id, onRemoveNote }) => {
       <textarea
         ref={textareaRef}
         value={content}
-        onChange={e => setContent(e.target.value)}
         className={`w-full h-full bg-transparent resize-none border-none focus:outline-none text-gray-900 overflow-hidden`}
         aria-label="Edit Note"
         placeholder="메모를 작성하세요."
