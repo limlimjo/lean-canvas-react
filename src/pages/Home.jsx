875671c47752119e1069aac048c30b080485e43a
limlimjo <skyblue.777@daum.net>
2025-05-07 22:08:14 +0900

    [add] 린 캔버스 검색 필터 카테고리 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,18 +6,28 @@ import { createCanvas, deleteCanvas, getCanvases } from '../api/canvas';
 import Loading from '../components/Loading';
 import Error from '../components/Error';
 import Button from '../components/Button';
+import CategoryFilter from '../components/CategoryFilter';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 
 function Home() {
-  const [searchText, setSearchText] = useState();
+  const [filter, setFilter] = useState({
+    searchText: undefined,
+    category: undefined,
+  });
+  const handleFilter = (key, value) =>
+    setFilter({
+      ...filter,
+      [key]: value,
+    });
   const [isGridView, setIsGridView] = useState(true);
 
   const queryClient = useQueryClient();
 
   // 1] 데이터 조회
   const { data, isLoading, error, refetch } = useQuery({
-    queryKey: ['canvases', searchText],
-    queryFn: () => getCanvases({ title_like: searchText }),
+    queryKey: ['canvases', filter.searchText, filter.category],
+    queryFn: () =>
+      getCanvases({ title_like: filter.searchText, category: filter.category }),
     initialData: [],
   });
 
@@ -50,7 +60,16 @@ function Home() {
   return (
     <>
       <div className="mb-6 flex flex-col sm:flex-row items-center justify-between">
-        <SearchBar searchText={searchText} setSearchText={setSearchText} />
+        <div className="flex gap-2 flex-col w-full sm:flex-row">
+          <SearchBar
+            searchText={filter.searchText}
+            onSearch={val => handleFilter('searchText', val)}
+          />
+          <CategoryFilter
+            category={filter.category}
+            onChange={val => handleFilter('category', val)}
+          />
+        </div>
         <ViewToggle isGridView={isGridView} setIsGridView={setIsGridView} />
       </div>
       <div className="flex justify-end mb-6">
@@ -64,7 +83,7 @@ function Home() {
         <CanvasList
           filteredItems={data}
           isGridView={isGridView}
-          searchText={searchText}
+          searchText={filter.searchText}
           onDeleteItem={handleDeleteItem}
         />
       )}
