f3f3d59d8c8895d355bc76c2f788608ca0e233f1
limlimjo <skyblue.777@daum.net>
2025-05-10 14:41:12 +0900

    [add] api server 환경변수 설정

--- src/api/http.js
+++ src/api/http.js
@@ -4,5 +4,13 @@ function create(baseURL, options) {
   const instance = axios.create(Object.assign({ baseURL }), options);
   return instance;
 }
-
-export const canvases = create('http://localhost:8000/canvases/');
+console.log(
+  'import.meta.env.VITE_API_BASE_URL: ',
+  import.meta.env.VITE_API_BASE_URL,
+);
+// export const canvases = create(
+//   'https://json-server-vercel-nine-psi.vercel.app/canvases/',
+// );
+export const canvases = create(
+  `${import.meta.env.VITE_API_BASE_URL}/canvases/`,
+);
